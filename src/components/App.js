26d0a21c85bf847d585420da2b502c3c21fddd9d
Sprat86 <spratlist@gmail.com>
2019-11-26 20:35:49 +0300

    Add "revert" function

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,32 @@
-import React from 'react'
+import React, {Component} from 'react'
 import ArticleList from './ArticleList/'
 import articles from '../fixtures'
 import 'bootstrap/dist/css/bootstrap.css'
 
-function App(){
+class App extends Component {
+	state = {
+		reverted: false
+	}
+
+	render () {
 	return(
 		<div className= 'contaner'>
 			<div className= 'jumbotron'>
-				<h1 className= 'display-3'>Articles list</h1>
+				<h1 className= 'display-3'>
+					Articles list
+					<button className="btn btn-outline-primary btn-lg ml-5" onClick = {this.revert}>Revert</button>
+				</h1>				
 			</div>	
-			<ArticleList articles = {articles} />
+			<ArticleList articles = {this.state.reverted ? articles.reverse() : articles} />
 		</div>
-	)
+		)
+	}
+
+	revert = () => {
+		this.setState({
+		reverted: !this.state.reverted
+		})
+	}
 }
 
 export default App
--- src/components/ArticleList/index.js
+++ src/components/ArticleList/index.js
@@ -3,10 +3,10 @@ import Article from '../Article'
 import './style.css'
 
 export default function ArticleList ( {articles} ) {
-	const articleElements = articles.map(item =>
+	const articleElements = articles.map((item, index) =>
 		// Передаем в компонент Article в качестве пропсов (article) объект item.
 			<li key = {item.id} className='article-list_li'>
-			<Article article = {item} />
+			<Article article = {item} defaultOpen = {index === 0} />
 			</li>
 	)
 	return (		
